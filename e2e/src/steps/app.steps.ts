74d1b663b5fdb9b01710f7413731b90dce5c45a4
Alvaro Lucas <45994280+alvz13@users.noreply.github.com>
2019-04-03 14:17:00 +0200

    Add

---
+++ e2e/src/steps/app.steps.ts
@@ -0,0 +1,19 @@
+    
+import { Before, Given, Then, When } from 'cucumber';
+import { expect } from 'chai';
+
+import { AppPage } from '../app.po';
+
+let page: AppPage;
+
+Before(() => {
+    page = new AppPage();
+});
+
+Given(/^Login page$/, { timeout: 4 * 800 }, async () => {
+  await page.navigateToLogin();
+});
+When(/^I do nothing$/, () => {});
+Then(/^Chat title$/, async () => {
+  expect(await page.getTitleText()).to.equal('Login Chat EN3A');
+});
